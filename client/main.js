927a38fb03b40e5d445dfa8bf4e883dc093b9b1e
Bridgitte <simone.p.bridgitte@hotmail.com>
2019-01-31 15:49:27 -0400

    added photo and updated css

--- client/main.js
+++ client/main.js
@@ -3,20 +3,12 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.profile.events({
+  'click .js-like'(event, instance) {
+  	console.log("You clicked like")
   },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  'click .js-dislike'(event, instance){
+  	alert("Dislike!? :'(");
+  }
 });
